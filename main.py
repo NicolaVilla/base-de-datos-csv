af65f335edb0c3ad3140eae68acdb2092fac792b
NicolaVilla <nicovillarpando@outlook.es>
2023-05-24 23:00:49 -0300

    refactor: :art: Crea estructura basica de la clase BDD

---
+++ main.py
@@ -0,0 +1,13 @@
+class BaseDeDatos():
+    def __init__(self,file_name):
+        self.path = "./archivos/" + file_name
+    
+    def ingresar_datos(self):
+
+    def crear_usuario(self):
+
+    def actualizar_usuario(self):
+
+    def borrar_usuario(self):
+    
+    def borrar_usuario(self):
